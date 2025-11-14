77bedcb1c8a1f94e6b671647030c124d30a6dbe2
chepchep2 <kajer102@gmail.com>
2025-11-14 23:15:00 +0900

    feat: findByEmail 메서드 추가

--- src/main/java/com/chep/demo/todo/domain/user/UserRepository.java
+++ src/main/java/com/chep/demo/todo/domain/user/UserRepository.java
@@ -2,6 +2,10 @@ package com.chep.demo.todo.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
