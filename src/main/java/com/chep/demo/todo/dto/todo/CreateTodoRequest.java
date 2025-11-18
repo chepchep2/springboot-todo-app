29e7e7b95659b0d8acb54ce856f6554e84acf3cd
chepchep2 <kajer102@gmail.com>
2025-11-18 18:21:29 +0900

    feat: 생성 요청 DTO에 담당자 추가하였습니다.

--- src/main/java/com/chep/demo/todo/dto/todo/CreateTodoRequest.java
+++ src/main/java/com/chep/demo/todo/dto/todo/CreateTodoRequest.java
@@ -3,11 +3,13 @@ package com.chep.demo.todo.dto.todo;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.time.Instant;
+import java.util.List;
 
 public record CreateTodoRequest(
         @NotEmpty(message = "Title is required")
         String title,
         String content,
         Integer orderIndex,
-        Instant dueDate
+        Instant dueDate,
+        List<Long> assigneeIds
 ) {}
