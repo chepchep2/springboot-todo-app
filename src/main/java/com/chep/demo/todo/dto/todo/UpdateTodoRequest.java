e2361dc8a27ed00d4c3fd5c0078c1b8d2aa73564
chepchep2 <kajer102@gmail.com>
2025-11-18 19:26:57 +0900

    refactor: due date는 따로 update하기 위해서 제거하였습니다.

--- src/main/java/com/chep/demo/todo/dto/todo/UpdateTodoRequest.java
+++ src/main/java/com/chep/demo/todo/dto/todo/UpdateTodoRequest.java
@@ -2,13 +2,10 @@ package com.chep.demo.todo.dto.todo;
 
 import jakarta.validation.constraints.NotEmpty;
 
-import java.time.Instant;
-
 public record UpdateTodoRequest(
         @NotEmpty(message = "Title is required")
         String title,
         String content,
-        Integer orderIndex,
-        Instant dueDate
+        Integer orderIndex
 ) {
 }
