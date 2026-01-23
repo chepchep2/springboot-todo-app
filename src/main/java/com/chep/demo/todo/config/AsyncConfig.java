8d4290d7ce77d67712b5fbf509de5e7ef9a79de6
chepchep2 <kajer102@gmail.com>
2026-01-24 01:48:40 +0900

    feat: Async 사용하기 위해서 설정 파일 등록

---
+++ src/main/java/com/chep/demo/todo/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.chep.demo.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
