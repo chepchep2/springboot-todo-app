04c4cb7b2cb17c00ad5cf04cbf12db7d8744f3d8
chepchep2 <kajer102@gmail.com>
2026-01-24 01:51:02 +0900

    feat: invitationId로 Invitation 조회하기 위해 JPQL 추가

--- src/main/java/com/chep/demo/todo/domain/invitation/InvitationRepository.java
+++ src/main/java/com/chep/demo/todo/domain/invitation/InvitationRepository.java
@@ -1,6 +1,8 @@
 package com.chep.demo.todo.domain.invitation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,4 +10,12 @@ import java.util.Optional;
 public interface InvitationRepository extends JpaRepository<Invitation, Long> {
     Optional<Invitation> findByInviteCodeCodeAndSentEmail(String code, String sentEmail);
     List<Invitation> findByInviteCodeWorkspaceIdAndSentEmailAndStatusIn(Long workspaceId, String sentEmail, List<Invitation.Status> statuses);
+    @Query("""
+            SELECT i
+            FROM Invitation i
+            JOIN FETCH i.inviteCode ic
+            JOIN FETCH ic.workspace w
+            WHERE i.id = :invitationId
+            """)
+    Optional<Invitation> findForEmailSend(@Param("invitationId") Long invitationId);
 }
